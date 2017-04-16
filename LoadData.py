26283786800c7a39e23b3d67415761b937b66077
OOC <whatisooc@gmail.com>
2017-04-16 23:07:00 +0800

    Updata LoadData.py

--- LoadData.py
+++ LoadData.py
@@ -2,30 +2,55 @@
 # -*- coding: utf-8 -*-
 import jieba
 import re
+import uniout  #让list正常输出中文，不显示unicode
 
-try:
-	f = open('./data/带标签短信.txt','r')
-	n = 1
-	if f:
-		print 'open file succeeed'
-	while n <6:  #600000
-		n = n + 1
-		line = f.readline()
-		print line
-		line = '1	344ddddd'
-		#if  re.match('^(\d{3})-(\d{3,8})$','010-12345'):
-		if re.match('(\d)\s\*',line):
-			print 'match succeed'
-		else:
-			print 'match error'
-		
-		m = re.match('(\d)\s\*',line)
-		print m.group(0)
-		#print m.group(1)
-		#print m.group(2)
-		
+def loadData():
+
+	postingList = []; classVec = []
+	try:
+		f = open('./data/带标签短信.txt','r')
+		n = 1
+		if f:
+			print 'open file succeeed'
+		while n <5:  #600000
+			n = n + 1
+			line = f.readline()
+			m = re.split('\t',line)  #返回list
+			#print str(m).decode('string_escape')
+			#print m #显示编码，无法正确显示中文
+			flag = int(m[0])  # str -> int
+			mess = m[1]  #str类型
+			messList = jieba.lcut(mess,cut_all=False)
+			#print str(messlist).decode('string_escape')
+			#print messlist #一条短信分词的list,没处理
+			postingList.append(messList)
+			classVec.append(flag)
+		#print classVec
+		#print postingList
+	finally:
+		if f:
+			f.close()
+	return postingList,classVec
+
 # jieba.lcut 直接返回list	
-finally:
-	if f:
-		f.close()
+def createVocabList(dataSet):
+	vocabSet = set([])
+	for doc in dataSet:
+		vocabSet = vocabSet | set(doc)
+	return list(vocabSet)
+def word2Vec(vocabList, inputSet):
+	returnVec = [0]*len(vocabList) #与词汇表等长的list
+	for word in inputSet:
+		if word in vocabList:
+			returnVec[vocabList.index(word)] = 1
+			#returnVec从0开始
+		else:
+			print "the word: %s is not in my \
+Vocabulary!" % word
+	return returnVec  #list 类型
+
+postingList,classVec = loadData() 
+vocabSet = createVocabList(postingList) #vocabSet是词汇表
+for doc in postingList:
+	print word2Vec(vocabSet, doc) #某条短信对应的词向量
 
--- data/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-head -n 10 带标签短信.txt > test.txt
-python -m jieba test.txt > test_cut.txt
